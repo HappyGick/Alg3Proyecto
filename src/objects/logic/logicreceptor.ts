5153c7d52ae836b794d9888dd6a027730a91c64f
AndyMine12 <andresale.fq@gmail.com>
2022-07-14 16:01:32 -0400

    Receptor (logic) and Neighborhood

---
+++ src/objects/logic/logicreceptor.ts
@@ -0,0 +1,37 @@
+import { Neighborhood } from "../logic/neighborhood";
+import { Observer, ObserverCallback } from "../observer";
+
+export class LogicReceptor<P,I>{
+    neighbors:Neighborhood<I,LogicReceptor<P,I>>;
+    piece:P; defaultPiece:P;
+    observer:Observer<P>;
+    
+    constructor(piece:P,defaultPiece:P){
+        this.piece = piece;
+        this.defaultPiece = defaultPiece;
+        this.neighbors = new Neighborhood<I,LogicReceptor<P,I>>();
+        this.observer = new Observer<P>(this.piece);
+    }
+
+    setNeighbors(n:Neighborhood<I,LogicReceptor<P,I>>){
+        this.neighbors = n;
+    }
+    addNeighbor(index:I,newNeighbor:LogicReceptor<P,I>){
+        this.neighbors.add(index,newNeighbor);
+    }
+    getNeighbor(index:I){
+        return this.neighbors.get(index);
+    }
+    placePiece(newPiece:P){
+        this.piece = newPiece;
+    }
+    clean(){
+        this.piece = this.defaultPiece;
+    }
+    updateObserver(){
+        this.observer.update(this.piece);
+    }
+    addSubscriber(newSubscriber:ObserverCallback<P>){
+        this.observer.subscribe(newSubscriber);
+    }
+}
---
+++ src/objects/logic/neighborhood.ts
@@ -0,0 +1,16 @@
+export class Neighborhood<I,E>{
+    maping:Map<I,E>;
+    constructor(){
+        this.maping = new Map<I,E>();
+    }
+
+    add(index:I,element:E):void{
+        this.maping.set(index,element);
+    }
+    get(index:I):E|undefined{
+        return this.maping.get(index); //May or may not include the index requested. Take into consideration as postcondition
+    }
+    includes(index:I):boolean{
+        return this.maping.has(index);
+    }
+}
